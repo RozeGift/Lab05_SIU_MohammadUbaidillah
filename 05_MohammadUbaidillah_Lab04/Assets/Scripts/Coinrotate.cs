7aee6c5a1447294fd2fb585308b0877458c1ca1e
RozeGift <72850017+RozeGift@users.noreply.github.com>
2022-01-20 12:51:20 +0800

    Project Added
    - Unity Folder Inserted Inside Repo

---
+++ 05_MohammadUbaidillah_Lab04/Assets/Scripts/Coinrotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coinrotate : MonoBehaviour
+{
+    float rotatespeed = 60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotatespeed * Time.deltaTime, 0);
+    }
+}
---
+++ 05_MohammadUbaidillah_Lab04/Assets/Scripts/Playermovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Playermovement : MonoBehaviour
+{
+    public float speed;
+    Rigidbody PlayerRb;
+
+    //coin text/score
+    public Text cointxt;
+    int coin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRb = GetComponent<Rigidbody>();
+        cointxt.text = "Coin Collected: " + coin;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveH, 0, moveV);
+        PlayerRb.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Hazards")
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+
+        if (collision.gameObject.tag == "Coin")
+        {
+            coin++;
+            cointxt.text = "Coin Collected: " + coin;
+            Destroy(collision.gameObject);
+
+            if(coin == 4)
+            {
+                SceneManager.LoadScene("GameWin");
+            }
+        }
+
+    }
+}
